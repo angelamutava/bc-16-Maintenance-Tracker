a76332924b9542a01172929522b18bf6b29dc16d
angieMutava <mutavaangela@yahoo.com>
2017-03-14 23:59:10 -0700

    added the signup and login functions

--- app/maintracker/views.py
+++ app/maintracker/views.py
@@ -9,4 +9,4 @@ from flask import render_template
 @main.route('/')
 @main.route('/home')
 def home_page():
-	return render_template('maintracker/base.html')
+	return render_template('maintracker/home.html')
