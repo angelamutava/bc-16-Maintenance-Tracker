741bb2084ffeadd87d8c9816eb81fe6b6ecca631
angieMutava <mutavaangela@yahoo.com>
2017-03-15 10:05:15 -0700

    Edited the form fields for item

---
+++ app/maintracker/forms.py
@@ -0,0 +1,16 @@
+from flask.ext.wtf import Form
+from wtforms import StringField, SelectField, SubmitField
+from wtforms import ValidationError
+from wtforms.validators import Required
+
+
+
+class MaintananceForm(Form):
+	item_name = StringField('Item Name', validators=[Required()])
+	item_issue = StringField('Item Issue', validators=[Required()])
+	item_type = StringField('Item Type', validators=[Required()])
+	status = SelectField(
+        'Status',
+        choices=[('good', 'Good'), ('needs repair', 'Needs Repair'), ('under repair', 'Under Repair')]
+    )
+	submit = SubmitField('Raise')
