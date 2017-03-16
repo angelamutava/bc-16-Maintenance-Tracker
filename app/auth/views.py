4046fb99e6efa9b97f338e8be52685db8abcee9c
angieMutava <mutavaangela@yahoo.com>
2017-03-16 06:55:21 -0700

    Added changes to functions in views

--- app/auth/views.py
+++ app/auth/views.py
@@ -32,7 +32,7 @@ def signup():
             email=user_form.email.data,
             phone_number=user_form.phone_number.data,
             password=user_form.password.data,
-            role="user")
+            role="admin")
         db.session.add(user)
         db.session.commit()
         return redirect(url_for('auth.login'))
